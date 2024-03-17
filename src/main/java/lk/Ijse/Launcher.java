b0112d40dc55a5243e7c4db3dc3833c43880e1d0
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-03-17 19:10:48 +0530

    first commit

---
+++ src/main/java/lk/Ijse/Launcher.java
@@ -0,0 +1,10 @@
+package lk.Ijse;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Launcher  {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
---
+++ src/main/java/lk/Ijse/Main.java
@@ -0,0 +1,20 @@
+package lk.Ijse;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+    launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/1.fxml"))));
+        stage.setTitle("Login Form");
+        stage.show();
+    }
+}
